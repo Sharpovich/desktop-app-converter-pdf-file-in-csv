ae48d93dd83e0fb6866573f335ae7cbd4761fa96
Timur Sharpov <57627185+Sharpovich@users.noreply.github.com>
2020-07-27 09:42:21 +0300

    Update EquipmentHelper.py

--- EquipmentHelper.py
+++ EquipmentHelper.py
@@ -4,14 +4,11 @@
 (C) 2019
 '''
 
-''' sys нужен для передачи argv в QApplication '''
-import sys
-''' import gui_EH импортирует наш интерфейс '''
-from gui_EH import *
+import sys # sys нужен для передачи argv в QApplication
+from gui_EH import * # import gui_EH импортирует наш интерфейс
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyPDF2 import PdfFileReader
-''' пакет tabula-py для обработки формата PDF '''
-import tabula
+import tabula # пакет tabula-py для обработки формата PDF
 
 class MyWin(QtWidgets.QMainWindow):
     ''' Класс MyWin объединяет главный файл с интерфейсом.
@@ -21,24 +18,22 @@ class MyWin(QtWidgets.QMainWindow):
         ''' модуль для доступа к переменным, методам в пакете gui_EH.py '''
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
-        ''' инициализации дизайна из пакета gui_EH '''
-        self.ui.setupUi(self)
-        ''' событие нажатия на кнопку '''
-        self.ui.pushButton.clicked.connect(self.DomainCheck)
-
-    ''' логика события взаимодействия с pushButton '''
+        self.ui.setupUi(self) # инициализации дизайна из пакета gui_EH
+        self.ui.pushButton.clicked.connect(self.DomainCheck) # событие нажатия на кнопку
+        
     def DomainCheck(self):
+        ''' логика события взаимодействия с pushButton '''
         while True:
             try:
-                ''' создание диалогового окна для ввода пользователем '''
+                # создание диалогового окна для ввода пользователем
                 str_, btn = QtWidgets.QInputDialog.getText(self, 'Ввод', 'Файл PDF:',
                                                            text='')
                 if btn:
                     x = str_
-                ''' выставляется флаг False при нажатии на close() и закрывается программа '''
+                # выставляется флаг False при нажатии на close() и закрывается программа
                 if btn == False:
                     break
-                ''' подсчёт количества страниц в файле '''
+                # подсчёт количества страниц в файле
                 with open(str(x) + '.pdf', 'rb') as f:
                     pdf = PdfFileReader(f)
                     number_of_pages = pdf.getNumPages()
@@ -47,8 +42,8 @@ class MyWin(QtWidgets.QMainWindow):
                     n = number_of_pages - 2
                 else:
                     n = number_of_pages - 1
-                ''' Алгоритм моделя из пакета Tabula-py '''
                 pageses = '1-' + str(n)
+                # Алгоритм моделя из пакета Tabula-py
                 tabula.convert_into(str(x) + '.pdf', 'Equipment.csv', output_format="csv", pages=pageses)
             except:
                 continue
@@ -56,11 +51,7 @@ class MyWin(QtWidgets.QMainWindow):
                 break
 
 if __name__=="__main__":
-    ''' Новый экземпляр QApplication '''
-    app = QtWidgets.QApplication(sys.argv)
-    ''' Создаём объект класса MyWin '''
-    myapp = MyWin()
-    ''' Показываем окно '''
-    myapp.show()
-    ''' и запускаем приложение '''
-    sys.exit(app.exec_())
+    app = QtWidgets.QApplication(sys.argv) # Новый экземпляр QApplication
+    myapp = MyWin() # Создаём объект класса MyWin
+    myapp.show() # Показываем окно
+    sys.exit(app.exec_()) # и запускаем приложение
